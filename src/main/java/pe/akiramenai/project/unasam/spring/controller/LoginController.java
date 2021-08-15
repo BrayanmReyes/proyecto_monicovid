13cebbad1d97e9933ae5d313673a641217016260
upc201711943 <55563776+upc201711943@users.noreply.github.com>
2021-08-14 19:40:14 -0500

    a ver si funciona logout

--- src/main/java/pe/akiramenai/project/unasam/spring/controller/LoginController.java
+++ src/main/java/pe/akiramenai/project/unasam/spring/controller/LoginController.java
@@ -1,10 +1,18 @@
 package pe.akiramenai.project.unasam.spring.controller;
 
 import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,4 +40,12 @@ public class LoginController {
 
 		return "login";
 	}
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+           new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+         return "redirect:/";
+     }
 }
